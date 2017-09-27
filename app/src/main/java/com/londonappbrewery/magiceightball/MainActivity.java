70e96b6ed9d1b455c2fe1b7543f98f588823c759
praveen <praveenhpk@gmail.com>
2017-09-27 23:23:43 +0530

    Coding Challenge #2 - Data Types, Variables and Arrays

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,13 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = findViewById(R.id.askButton);
+
+        final ImageView ballImage = findViewById(R.id.imageView);
+        final int[] ballArray = {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
+
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumber = new Random();
+                int num = randomNumber.nextInt(5);
+                Log.d("Ball","Button pressed :"+num);
+                ballImage.setImageResource(ballArray[num]);
+    }
+        });
     }
 }
